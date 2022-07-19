0b53a086ea84488941ca2703662d45ee0e918051
markovaleksandr108@gmail.com <markovaleksandr108@gmail.com>
2022-07-19 22:07:47 +0300

    Add object slider.

--- src/slider.js
+++ src/slider.js
@@ -1,1 +1,10 @@
-console.log('#');
+let slider = {
+  slides: ['1.jpg', '2.jpg', '3.jpg'],
+  frame: 0,
+  set: function(image) {
+    let path = "url(" + image + ")";
+    console.log(path);
+    document.getElementById('scr').style.backgroundImage = path;
+    document.getElementById('scr').classList.toggle('animated');
+  },
+};
